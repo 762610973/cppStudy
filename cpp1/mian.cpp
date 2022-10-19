58d84fe253115bf0b4522187a598a69ba4dbcfbe
XiaoLiang <762610973@qq.com>
2022-10-19 11:50:14 +0800

    简单提交一下

---
+++ cpp1/mian.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "hello world" << endl;
+    
+    int i = 0;
+    i++ + ++i;
+    cout << i;
+    return 0;
+
+}
---
+++ node.md
@@ -0,0 +1,3 @@
+# cppStudy
+
+## start
